5ebda88209e5ba2fbba5cf8d4f7abda0bbbf4dc1
Yun-Chih Yang <yun-chih.yang@inter.ikea.com>
2022-10-12 14:31:12 +0200

    Add a new cypress test file

---
+++ cypress/e2e/1-getting-started/test_screenshot.cy.js
@@ -0,0 +1,13 @@
+describe('Visual regression tests', {
+  retries: 2
+}, () => {
+  [assembly_instructions].forEach((test_name) => {
+    it(`Should match previous screenshot "${test_name}"`, () => {
+      // 'https://viewer:ipex@ipex-viewer.azurewebsites.net/main/#/assembly_instructions/'
+
+      cy.visit('https://viewer:ipex@ipex-viewer.azurewebsites.net/main/#/assembly_instructions/')
+
+      cy.wait(1500)
+    })
+  })
+})
