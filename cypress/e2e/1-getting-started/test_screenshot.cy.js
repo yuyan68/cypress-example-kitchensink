ae33df24dc3a0829af95d9bc034062d05060e12f
Yun-Chih Yang <yun-chih.yang@inter.ikea.com>
2022-10-12 16:13:05 +0200

    Fixed cypress test

--- cypress/e2e/1-getting-started/test_screenshot.cy.js
+++ cypress/e2e/1-getting-started/test_screenshot.cy.js
@@ -1,7 +1,7 @@
 describe('Visual regression tests', {
   retries: 2
 }, () => {
-  [assembly_instructions].forEach((test_name) => {
+  ['assembly_instructions'].forEach((test_name) => {
     it(`Should match previous screenshot "${test_name}"`, () => {
       // 'https://viewer:ipex@ipex-viewer.azurewebsites.net/main/#/assembly_instructions/'
 
